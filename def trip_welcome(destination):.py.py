45cdb585907420cf54aef423d58fc3355b555599
imfeelinsoggood <imfeelinsoggood@gmail.com>
2024-09-24 14:06:47 +0300

    parametrs and arguments of the function

---
+++ def trip_welcome(destination):.py.py
@@ -0,0 +1,7 @@
+def trip_welcome(destination): #вказуємо,що destination буде вводитися пізніше користувачем
+  print("Welcome to Tripcademy!") 
+  print("Looks like you're going to " + destination + " today.")
+
+trip_welcome("kyiv")  #викликаємо фунцію,прописуємо данні,які мають бути використані у блоці
+
+
