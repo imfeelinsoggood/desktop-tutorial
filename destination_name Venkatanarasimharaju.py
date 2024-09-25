b322a308c1c451bff7591fe18fb15b8bfb21bf7a
imfeelinsoggood <imfeelinsoggood@gmail.com>
2024-09-25 16:11:02 +0300

    learning_how_to_use_round_function

---
+++ destination_name Venkatanarasimharaju.py
@@ -0,0 +1,10 @@
+
+
+destination_name = "Venkatanarasimharajuvaripeta"
+
+# Built-in function: len()
+length_of_destination = len(destination_name)
+
+# Built-in function: print()
+print(length_of_destination)
+
